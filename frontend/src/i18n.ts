b5419d0fd710798c57ca4d497249300c2cfe17e3
lukashow <howweishan@slimer.dev>
2026-01-22 02:16:40 +0800

    chore(i18n): update more strings for both zh and en translation

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,16 +1,24 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './locales/en.json'
 import zh from './locales/zh.json'
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    zh: { translation: zh }
-  },
-  lng: 'zh',
-  fallbackLng: 'en',
-  interpolation: { escapeValue: false }
-})
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      zh: { translation: zh }
+    },
+    // lng: 'zh', // Remove hardcoded language to let detector work
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage']
+    }
+  })
 
 export default i18n
